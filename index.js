1bdcf15328f6c2f68ae7d295216329babcaf5299
Sumit chaudhary <sumitchaudhary@192.168.1.8>
2022-05-05 11:14:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=> res.send('hello world'))
+app.get('/home',(req,res)=>res.send('oh hi home'))
+app.listen(3000,()=>console.log('workings'))
